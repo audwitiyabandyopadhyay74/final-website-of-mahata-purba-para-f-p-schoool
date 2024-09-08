17c058e1eba2ed471e5df8a0b756a08788e871e3
Audwitiya Bandyopadhyay <audwitiyabandyopadhyay74@outlook.com>
2024-09-08 12:56:27 +0530

    faviucon

--- final-touch/app/layout.tsx
+++ final-touch/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+// import favicon from "./pages/favicon.ico";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="shortcut icon" href={"/app/favicon.ico.png"} type="image/x-icon" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
