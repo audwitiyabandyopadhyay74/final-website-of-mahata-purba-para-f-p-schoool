1026c74f25b21c65612eddd07916399eaf44190a
Audwitiya Bandyopadhyay <audwitiyabandyopadhyay74@outlook.com>
2024-09-08 13:04:46 +0530

    Update layout.tsx

--- final-touch/app/layout.tsx
+++ final-touch/app/layout.tsx
@@ -17,9 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="shortcut icon" href={"/app/favicon.ico.png"} type="image/x-icon" />
-      </head>
+      
       <body className={inter.className}>{children}</body>
     </html>
   );
