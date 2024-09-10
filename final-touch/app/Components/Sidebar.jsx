981933d43a3cf0ec5297638752d17092c3e01d2e
Audwitiya Bandyopadhyay <audwitiyabandyopadhyay74@outlook.com>
2024-09-10 20:38:26 +0530

    Update Sidebar.jsx

--- final-touch/app/Components/Sidebar.jsx
+++ final-touch/app/Components/Sidebar.jsx
@@ -1,39 +1,48 @@
-"use client"
-import React from 'react'
-import {app} from "../firebaseConfig"
-import { getAuth, signOut } from "firebase/auth";
+// "use client"
+// import React from 'react'
+// import {app} from "../firebaseConfig"
+// import { getAuth, signOut } from "firebase/auth";
 
-const Sidebar = () => {
-  // var [style, setStyle] = useState({})
-  // console.log(style)
-  // const styleh = {
-  //   borderRightColor: `${style}`
-  // }
+// const Sidebar = () => {
+//   // var [style, setStyle] = useState({})
+//   // console.log(style)
+//   // const styleh = {
+//   //   borderRightColor: `${style}`
+//   // }
 
-  const auth = getAuth(app);
+//   const auth = getAuth(app);
 
-  const signOutUser = () =>{
-    signOut(auth).then(() => {
-      alert("signed out")
-    }).catch((error) => {
-      console.log(error)
-    });
-  }
+//   const signOutUser = () =>{
+//     signOut(auth).then(() => {
+//       alert("signed out")
+//     }).catch((error) => {
+//       console.log(error)
+//     });
+//   }
   
 
-  return (
-    <div className='sidebar'>
-      <div className="logo">
-        Mahata Purba Para Free Primary School H.T Portal
-      </div>
-      <div className="options">
-      <div className='option'> Dashboard</div>
-        <div className='option'>Teacher Attendance</div>
-        <div className="option">Current School Fund</div>
-      <button onClick={signOutUser} className='h-[7vh] w-[20vh] bg-black text-white border-none outline-none rounded-md shadow-lg p-1 font-bold hover:scale-110'>Sign Out</button>
-      </div>
-      </div>
+//   return (
+//     <div className='sidebar'>
+//       <div className="logo">
+//         Mahata Purba Para Free Primary School H.T Portal
+//       </div>
+//       <div className="options">
+//       <div className='option'> Dashboard</div>
+//         <div className='option'>Teacher Attendance</div>
+//         <div className="option">Current School Fund</div>
+//       <button onClick={signOutUser} className='h-[7vh] w-[20vh] bg-black text-white border-none outline-none rounded-md shadow-lg p-1 font-bold hover:scale-110'>Sign Out</button>
+//       </div>
+//       </div>
     
+//   )
+// }
+
+// export default Sidebar
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div>Sidebar</div>
   )
 }
 
