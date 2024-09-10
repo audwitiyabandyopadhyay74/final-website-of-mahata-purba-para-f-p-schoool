263565ae2789fb66047e14e48fb9e711398f176f
Audwitiya Bandyopadhyay <audwitiyabandyopadhyay74@outlook.com>
2024-09-10 15:09:04 +0530

    Update page.jsx

--- final-touch/app/teacher-portal/page.jsx
+++ final-touch/app/teacher-portal/page.jsx
@@ -1,43 +1,53 @@
-"use client"
-import React, { useEffect, useState } from 'react'
+// "use client"
+// import React, { useEffect, useState } from 'react'
 
-import Sidebar from '../Components/Sidebar'
-import Dashboard from '../Components/Dashboard'
-import { app } from '../firebaseConfig'
-import { onAuthStateChanged } from 'firebase/auth'
-import { getAuth } from "firebase/auth";
+// import Sidebar from '../Components/Sidebar'
+// import Dashboard from '../Components/Dashboard'
+// import { app } from '../firebaseConfig'
+// import { onAuthStateChanged } from 'firebase/auth'
+// import { getAuth } from "firebase/auth";
 
-export default function Page() {
-  const auth = getAuth(app)
-  const [user1, setUser] = useState(false);
+// export default function Page() {
+//   const auth = getAuth(app)
+//   const [user1, setUser] = useState(false);
 
-  useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setUser(true)
-      } else {
-        setUser(false)
-      }
-    });
-  }, [auth])
-const onLogin=()=>{
- document.location.href="/teacher-login"
-}
+//   useEffect(() => {
+//     onAuthStateChanged(auth, (user) => {
+//       if (user) {
+//         setUser(true)
+//       } else {
+//         setUser(false)
+//       }
+//     });
+//   }, [auth])
+// const onLogin=()=>{
+//  document.location.href="/teacher-login"
+// }
 
+//   return (
+//     <> 
+//       {user1 ? (
+//         <div className='flex'>
+//           <Sidebar />
+//           <Dashboard />
+//         </div>
+//       ) : (
+//         <div className=' w-screen h-screen flex items-center content-center'>
+//           {/* You can add a login component or a message here */}
+// <h1 className='text-5xl font-bold px-[50%] w-[400vh]'>Please Login To Access HT Portal</h1>
+// <button className='' onClick={onLogin}>Login</button>
+//         </div>
+//       )}
+//     </>
+//   );
+// }
+
+import React from 'react'
+
+const page = () => {
   return (
-    <> 
-      {user1 ? (
-        <div className='flex'>
-          <Sidebar />
-          <Dashboard />
-        </div>
-      ) : (
-        <div className=' w-screen h-screen flex items-center content-center'>
-          {/* You can add a login component or a message here */}
-<h1 className='text-5xl font-bold px-[50%] w-[400vh]'>Please Login To Access HT Portal</h1>
-<button className='' onClick={onLogin}>Login</button>
-        </div>
-      )}
-    </>
-  );
+    <div>page</div>
+  )
 }
+
+export default page
