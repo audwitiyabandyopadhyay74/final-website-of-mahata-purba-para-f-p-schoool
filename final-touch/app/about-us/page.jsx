96216008091e09431dabcc3b7a2a7afc1496c116
Audwitiya Bandyopadhyay <audwitiyabandyopadhyay74@outlook.com>
2024-09-11 10:51:34 +0530

    Update page.jsx

--- final-touch/app/about-us/page.jsx
+++ final-touch/app/about-us/page.jsx
@@ -1,9 +1,8 @@
-// import Footer from "../Components/Footer.jsx";
 import Aboutus from "../Pages/About-us";
 import Navbar from "../Components/Navbar";
 import NavbarForMoblie from "../Components/Navbar For Mobile";
 import Footer from "../Components/Footer";
-// import AboutTeachers from "../Components/About Teachers";
+
 
 const AboutPage = () => {
   return (
